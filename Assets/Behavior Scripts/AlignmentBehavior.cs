c9046cd22314d4d5a131793ae97a3c972a23593f
Giggums <101183500+Giggums@users.noreply.github.com>
2022-04-25 13:15:58 +0100

    gg

--- Assets/Behavior Scripts/AlignmentBehavior.cs
+++ Assets/Behavior Scripts/AlignmentBehavior.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Flock/Behavior/Alignment")]
 public class AlignmentBehavior : FlockBehavior
 {
+	Vector2 m_NewPosition;
 	public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
 	{
+		m_NewPosition = new Vector2(Random.Range(0,1f), Random.Range(0, 1f));
 		//if no neighbors, maintain current alignment
 		if (context.Count == 0)
 			return agent.transform.up;
@@ -16,7 +18,7 @@ public class AlignmentBehavior : FlockBehavior
 		Vector2 alignmentMove = Vector2.zero;
 		foreach (Transform item in context)
 		{
-			alignmentMove += (Vector2)item.transform.up;
+			alignmentMove += (Vector2)item.transform.up; //+ m_NewPosition;
 		}
 		alignmentMove /= context.Count;
 
